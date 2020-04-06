8ea7f52e4883ce0ec58f1b7ba5230d77cca549c2
enteraether <thetheresagarcia@gmail.com>
2020-04-06 17:40:19 -0400

    still playing w form

--- screens/AddNewGoal.js
+++ screens/AddNewGoal.js
@@ -1,56 +1,78 @@
 import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TextInput, Button } from 'react-native';
 import { globalStyles } from '../styles/global.js';
-import { Formik } from 'formik';
+import {   
+  Formik,
+  Field,
+  Form,
+  useField,
+  FieldAttributes,
+  FieldArray} 
+from 'formik';
+import {
+  TextField,
+  Checkbox,
+  Radio,
+  FormControlLabel,
+  Select,
+  MenuItem
+} from "@material-ui/core";
 
-export default function AddNewGoal({navigation}) {
+create_table "goals", force: :cascade do |t|
+t.integer "likes"
+t.boolean "private"
+t.string "what"
+t.string "why"
+t.string "name"
+t.datetime "start_date"
+t.bigint "user_id", null: false
+t.datetime "created_at", precision: 6, null: false
+t.datetime "updated_at", precision: 6, null: false
+t.index ["user_id"], name: "index_goals_on_user_id"
+end
+
+export default function AddNewGoal(props) {
     // const { navigation } = props
   return (
     <View style={globalStyles.container}>
-<Formik
-        initialValues={{ title: '', body: '', rating: '' }}
+    <Formik
+        initialValues={{private: '', what: '', why: '', name: '', start_date: '' }}
         onSubmit={(values, actions) => {
           actions.resetForm(); 
-          addReview(values);
+          // addReview(values);
         }}
-      >
+    >
         {props => (
+  
           <View>
+            <Text >Goal Name</Text>
             <TextInput
               style={globalStyles.formInput}
-              placeholder='Review title'
-              onChangeText={props.handleChange('title')}
-              value={props.values.title}
+              placeholder='name'
+              onChangeText={props.handleChange('name')}
+              value={props.values.name}
             />
-
+            <Text >What daily task will you do to reach your goal?</Text>
             <TextInput
               style={globalStyles.formInput}
               multiline
-              placeholder='Review details'
-              onChangeText={props.handleChange('body')}
-              value={props.values.body}
+              placeholder='Daily Task'
+              onChangeText={props.handleChange('what')}
+              value={props.values.what}
             />
-
+            <Text >Why do you want to accomplish this goal? How will it make you better? How will you see yourself differently once you accomplish this goal?</Text>
             <TextInput 
               style={globalStyles.formInput}
-              placeholder='Rating (1 - 5)'
-              onChangeText={props.handleChange('rating')}
-              value={props.values.rating}
-              keyboardType='numeric'
+              placeholder='Why'
+              onChangeText={props.handleChange('why')}
+              value={props.values.why}
+              // keyboardType='numeric'
             />
             
             <Button color='maroon' title="Submit" onPress={props.handleSubmit} /> 
           </View>
         )}
       </Formik>
-
-      {/* <Text style={globalStyles.titleText}>User's Individ Goal </Text>
-      <Text style={globalStyles.titleText}>____ Consecutive Days Practicing Goal </Text>
-      <Text style={globalStyles.titleText}>____ Total Days Practicing Goal </Text>
-      <Text style={globalStyles.titleText}>Log Today's Progress </Text>
-      <Text style={globalStyles.titleText}>____ Total Days Practicing Goal </Text> */}
-
-
     </View>
   );
 }
